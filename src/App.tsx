452775159c2051cb620e41dcddb7ea70d1839d48
suriya babaei <suriyalifework60@gmail.com>
2024-04-30 11:31:55 +0330

    add main and herosection and notfound

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-export default function App() {
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // <Provider store={store}>
     <App />
-)
+  // </Provider>
+);
