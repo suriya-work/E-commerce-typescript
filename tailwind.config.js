6340eeb4c2eee9b27c0a7ea99982db51658bd605
suriya babaei <suriyalifework60@gmail.com>
2024-04-30 10:08:04 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'white': '#fff',
+        'primery': "#4E66EC",
+        'green': '#487664',
+        'myRed': '#969696',
+        'Red': '#ff0000',
+        'RedLight': '#A71B4A',
+      },
+      fontFamily: {
+        myFont: "Roboto, sans-serif",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        lg: '3rem',
+        xl: '4rem',
+      }
+    },
+  },
+  plugins: [],
+}
